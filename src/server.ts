368d83543282aa11625af15cb5537b25cbfe2d31
Juan Lucas Ormelli de Medeiros <juan_ormelli@hotmail.com>
2021-08-16 11:10:53 -0300

    Adicionando TypeOrm

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express, { request } from "express";
 import { router } from "./routes";
 import swaggerUI from "swagger-ui-express";
 import swaggerFile from "./swagger.json";
-
+import "./database"
 const app = express();
 app.use(express.json())
 app.use("/api-docs", swaggerUI.serve,swaggerUI.setup(swaggerFile))
