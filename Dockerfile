e5a82df1a37a2a02226b08122299c1042662d368
Juan Lucas Ormelli de Medeiros <juan_ormelli@hotmail.com>
2021-08-30 13:29:58 -0300

    criando tabela de carros

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,10 @@
 FROM node:14
 
-
-
-
 WORKDIR /usr/app
 
 COPY package.json ./
 
 RUN npm install
-
 
 COPY . .
 
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,11 +2,13 @@ import { createConnection, getConnectionOptions } from "typeorm";
 
 interface IOptions {
   host: string;
+  name: string;
 }
 
 getConnectionOptions().then((options) => {
   const newOptions = options as IOptions;
   newOptions.host = "database_ignite";
+  newOptions.name ="juan",
   createConnection({
     ...options,
   });
