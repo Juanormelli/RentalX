a443646d5c87f06ad113d11a32b1387976693dae
Juan Lucas Ormelli de Medeiros <juan_ormelli@hotmail.com>
2021-10-18 14:12:40 -0300

    Sentry

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -13,12 +13,31 @@ import "../container";
 import  cors  from 'cors';
 import rateLimiter from "../http/middleware/rateLimiter"
 
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
+
 
 createConnection();
 const app = express();
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+ 
+  tracesSampleRate: 1.0,
+});
+
 app.use(rateLimiter);
 app.use(express.json());
 app.use(cors())
+
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 
 
@@ -28,6 +47,18 @@ app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`))
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
 
 app.use(router);
+
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      // Capture all 404 and 500 errors
+      if (error.status === 429 || error.status === 500) {
+        return true;
+      }
+      return false;
+    },
+  })
+);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if (err instanceof AppError) {
